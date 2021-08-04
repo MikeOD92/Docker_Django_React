81e7c426705bb0232e23bc4ff3969e18077a8d01
Michael <michaelodell660@gmail.com>
2021-08-04 12:24:29 -0700

    working on serializers views and roles

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from users.serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import exceptions, serializers, viewsets
+from rest_framework import exceptions, viewsets, status
 from rest_framework.views import APIView
 
 from .serializers import RoleSerializer, UserSerializer, PermissionSerializer 
@@ -94,7 +94,13 @@ class RoleViewSet(viewsets.ViewSet):
         })
 
     def create(self, request):
-        pass
+        serializer = RoleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({
+            'data': serializer.data
+        }, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         pass
